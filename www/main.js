4cd5644c3130139cd7414716c5932af140794e3d
Tran Gia Quyen <giaquyen.tran@gmail.com>
2020-07-01 16:19:12 +0700

    main.js

--- www/main.js
+++ www/main.js
@@ -14,3 +14,18 @@ function start(){
 
 }
 
+function start(){
+    document.getElementById("typing").disabled = false;
+    if(s == -1){
+        clearTimeout(timeout);
+        //alert("time out");
+        submit();
+        return false;
+    }
+    document.getElementById('s').innerText = s.toString();
+    timeout = setTimeout(function(){
+        s--;
+        start();
+    },1000);
+
+}
